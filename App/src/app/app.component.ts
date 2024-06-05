db36c1506a88462d2440a4d5e8108e40eb8bcd53
ispg2022102220 <ispg2022102220@ispgaya.pt>
2024-06-05 12:38:46 +0100

    3

--- App/src/app/app.component.ts
+++ App/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FormsModule,],
+  imports: [RouterOutlet, FormsModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -25,10 +26,128 @@ export class AppComponent
   bike_pace : TriathlonPace[] = [new TriathlonPace(0)];
   run_pace : TriathlonPace[] = [new TriathlonPace(0)];
 
+  total_time : TriathlonTime = new TriathlonTime(0,0,0);
+  goal_result : TriathlonGoal = new TriathlonGoal(0, 0, 0, this.total_time);
+
   swim_distance = this.distances[0]['swim'];
   bike_distance = this.distances[0]['bike'];
   run_distance = this.distances[0]['run'];
+
+  //Flags that change css
+  buttonClicked_sprint = false;
+  buttonClicked_olympic = false;
+  buttonClicked_halfIronman = false;
+  buttonClicked_ironman = false;
+  showWarning = false;
+
+  showFeedback = false;
+
+  toggleFeedback() {
+    const swim_seconds = this.swim_time[0].hour * 3600 + this.swim_time[0].minute * 60 + this.swim_time[0].second > 0;
+    const bike_seconds = this.bike_time[0].hour * 3600 + this.bike_time[0].minute * 60 + this.bike_time[0].second > 0;
+    const run_seconds = this.run_time[0].hour * 3600 + this.run_time[0].minute * 60 + this.run_time[0].second > 0;
+    const interval_01 = this.interval_01[0]['minute'] * 60 + this.interval_01[0]['second'] > 0;
+    const interval_02 = this.interval_02[0]['minute'] * 60 + this.interval_02[0]['second'] > 0;
+
+    if (!this.showFeedback) {
+      if (swim_seconds && bike_seconds && run_seconds && interval_01 && interval_02) {
+        this.showFeedback = !this.showFeedback;
+      }
+    } else {
+      this.showFeedback = !this.showFeedback;
+    }
+  }
   
+  toggleWarning() {
+    this.showWarning = !this.showWarning;
+  }
+
+  toggleButton(type : String) {
+    switch (type) {
+      case 'sprint':
+        this.buttonClicked_sprint = true;
+        this.buttonClicked_olympic = false;
+        this.buttonClicked_halfIronman = false;
+        this.buttonClicked_ironman = false;
+        break;
+      case 'olympic':
+        this.buttonClicked_sprint = false;
+        this.buttonClicked_olympic = true;
+        this.buttonClicked_halfIronman = false;
+        this.buttonClicked_ironman = false;
+        break;
+      case 'halfIronman':
+        this.buttonClicked_sprint = false;
+        this.buttonClicked_olympic = false;
+        this.buttonClicked_halfIronman = true;
+        this.buttonClicked_ironman = false;
+        break;
+      case 'ironman':
+        this.buttonClicked_sprint = false;
+        this.buttonClicked_olympic = false;
+        this.buttonClicked_halfIronman = false;
+        this.buttonClicked_ironman = true;
+        break;
+      case 'deactivate':
+        this.buttonClicked_sprint = false;
+        this.buttonClicked_olympic = false;
+        this.buttonClicked_halfIronman = false;
+        this.buttonClicked_ironman = false;
+        break;
+      default:
+        this.buttonClicked_sprint = false;
+        this.buttonClicked_olympic = false;
+        this.buttonClicked_halfIronman = false;
+        this.buttonClicked_ironman = false;
+        break;
+    }
+  }
+
+  setGoal() {
+    const distance = {
+      swim: this.swim_distance,
+      bike: this.bike_distance,
+      run: this.run_distance,
+    }
+
+    const time = {
+      swim: this.swim_time,
+      bike: this.bike_time,
+      run: this.run_time,
+    }
+
+    const pace = {
+      swim: this.swim_pace,
+      bike: this.bike_pace,
+      run: this.run_pace,
+    }
+
+    this.goal_result['distance'] = distance;
+    this.goal_result['time'] = time;
+    this.goal_result['pace'] = pace;
+
+    console.log(this.goal_result);
+  }
+
+  totalTimeUpdate() {
+    const swim_seconds = this.swim_time[0].hour * 3600 + this.swim_time[0].minute * 60 + this.swim_time[0].second;
+    const bike_seconds = this.bike_time[0].hour * 3600 + this.bike_time[0].minute * 60 + this.bike_time[0].second;
+    const run_seconds = this.run_time[0].hour * 3600 + this.run_time[0].minute * 60 + this.run_time[0].second;
+    const interval_01 = this.interval_01[0]['minute'] * 60 + this.interval_01[0]['second'];
+    const interval_02 = this.interval_02[0]['minute'] * 60 + this.interval_02[0]['second'];
+
+    const total_seconds = swim_seconds + bike_seconds + run_seconds + interval_01 + interval_02;
+
+    const hours = Math.floor(total_seconds / 3600);
+    const remainingSeconds = total_seconds % 3600;
+    const minutes = Math.floor(remainingSeconds / 60);
+    const seconds = Math.round(remainingSeconds % 60);
+
+    this.total_time.hour = hours;
+    this.total_time.minute = minutes;
+    this.total_time.second = seconds;
+  }
+
   valueChange()
   {
     this.checkAndSetDefault_interval(this.interval_01);
@@ -53,45 +172,100 @@ export class AppComponent
     this.bike_distance = this.distances[0]['bike'];
     this.run_distance = this.distances[0]['run'];
 
+    this.toggleButton('deactivate');
+
     this.update_bike_pace();
     this.update_run_pace();
     this.update_swim_pace();
   }
 
-  update_bike_pace() {
-    const seconds = this.bike_time[0]['hour'] * 3600 + this.bike_time[0]['minute'] * 60 + this.bike_time[0]['second'];  
+  // Pace input changed
+  update_bike_time() {
+    const pace_seconds = this.bike_pace[0]['speed'];
+    const total_seconds = this.bike_distance / pace_seconds;
+  
+    const hours = Math.floor(total_seconds / 3600);
+    const remainingSeconds = total_seconds % 3600;
+    const minutes = Math.floor(remainingSeconds / 60);
+    const seconds = Math.round(remainingSeconds % 60);
+  
+    this.bike_time[0]['hour'] = hours;
+    this.bike_time[0]['minute'] = minutes;
+    this.bike_time[0]['second'] = seconds;
+  }
 
-    if (seconds > 0) {
-      this.bike_pace[0]['speed'] = Math.floor(this.bike_distance / seconds);
+  // Time input changed
+  update_bike_pace() {
+    const seconds = this.bike_time[0]['hour'] * 3600 + this.bike_time[0]['minute'] * 60 + this.bike_time[0]['second']; 
+    const num = this.bike_distance / seconds; 
+
+    if (seconds > 0 && this.bike_distance != 0) {
+      this.bike_pace[0]['speed'] = parseFloat(num.toFixed(2));
     } else {
       this.bike_pace[0]['speed'] = 0;
     }
+
+    this.totalTimeUpdate();
+  }
+
+  update_run_time() {
+    const pace_seconds = this.run_pace[0]['speed'];
+    const total_seconds = this.run_distance / pace_seconds;
+  
+    const hours = Math.floor(total_seconds / 3600);
+    const remainingSeconds = total_seconds % 3600;
+    const minutes = Math.floor(remainingSeconds / 60);
+    const seconds = Math.round(remainingSeconds % 60);
+  
+    this.run_time[0]['hour'] = hours;
+    this.run_time[0]['minute'] = minutes;
+    this.run_time[0]['second'] = seconds;
   }
 
   update_run_pace() {
-    const hours = this.run_time[0]['hour'] + this.run_time[0]['minute'] / 60 + this.run_time[0]['second'] / 3600;
+    const seconds = this.run_time[0]['hour'] * 3600 + this.run_time[0]['minute'] * 60 + this.run_time[0]['second']; 
+    const num = this.run_distance / seconds; 
 
-    if (hours > 0) {
-      this.run_pace[0]['speed'] = Math.floor(this.run_distance / hours);
+    if (seconds > 0 && this.run_distance != 0) {
+      this.run_pace[0]['speed'] = parseFloat(num.toFixed(2));
     } else {
       this.run_pace[0]['speed'] = 0;
-    }
+    } 
+
+    this.totalTimeUpdate();
+  } 
+
+  update_swim_time() {
+    const pace_seconds = (this.swim_pace[0]['minute'] * 60 + this.swim_pace[0]['second']) / 50; // Convert pace to seconds per swim distance
+    const total_seconds = this.swim_distance * pace_seconds; // Total seconds for swim distance
+  
+    const hours = Math.floor(total_seconds / 3600); // Calculate hours
+    const remainingSeconds = total_seconds % 3600;
+    const minutes = Math.floor(remainingSeconds / 60); // Calculate minutes
+    const seconds = Math.round(remainingSeconds % 60); // Calculate remaining seconds
+  
+    this.swim_time[0]['hour'] = hours;
+    this.swim_time[0]['minute'] = minutes;
+    this.swim_time[0]['second'] = seconds;
   }
 
   update_swim_pace() {
-    let total_minutes = this.swim_time[0]['hour'] * 60 + this.swim_time[0]['minute'] + this.swim_time[0]['second'] / 60;
-    total_minutes /= 50;
-    const minutes = Math.floor(total_minutes);
-    const seconds = Math.round((total_minutes - minutes)*60);
+    const total_seconds = this.swim_time[0]['hour'] * 3600 + this.swim_time[0]['minute'] * 60 + this.swim_time[0]['second'];
+    const num_segments = this.swim_distance / 50;
+    const pace_per_50m_seconds = total_seconds / num_segments;
 
-    if (total_minutes > 0) {
+    const minutes = Math.floor(pace_per_50m_seconds / 60);
+    const seconds = Math.round(pace_per_50m_seconds % 60);
+
+    if (pace_per_50m_seconds > 0 && this.swim_distance != 0) {
       this.swim_pace[0]['minute'] = minutes;
       this.swim_pace[0]['second'] = seconds;
-      console.log(total_minutes);
     } else {
       this.swim_pace[0]['minute'] = 0;
       this.swim_pace[0]['second'] = 0;
     }
+
+    this.totalTimeUpdate();
   }
 
   checkAndSetDefault_time(times: TriathlonTime[]) {
@@ -142,15 +316,19 @@ export class AppComponent
     {
       case 'sprint':
         this.distances.push(new TriathlonDistance(750, 20000, 5000));
+        this.toggleButton('sprint');
         break;
       case 'olympic':
         this.distances.push(new TriathlonDistance(1500, 40000, 10000));
+        this.toggleButton('olympic');
         break;
       case 'halfIronman':
         this.distances.push(new TriathlonDistance(1900, 90000, 21100));
+        this.toggleButton('halfIronman');
         break;
       case 'ironman':
         this.distances.push(new TriathlonDistance(3800, 180000, 42195));
+        this.toggleButton('ironman');
         break;
       default:
         this.distances.length=0;
@@ -161,10 +339,19 @@ export class AppComponent
     this.bike_distance = this.distances[0]['bike'];
     this.run_distance = this.distances[0]['run'];
 
+    this.update_swim_pace();
     this.update_bike_pace();
+    this.update_run_pace();
   }
 }
 
+class TriathlonGoal
+{
+  constructor(public distance : Object, public time : Object, public pace : Object, public total_time : TriathlonTime) 
+  {
+    
+  }
+}
 
 class TriathlonDistance
 {
@@ -190,4 +377,3 @@ class TriathlonPace_swim
 {
   constructor(public minute : number, public second : number) { }
 }
-
