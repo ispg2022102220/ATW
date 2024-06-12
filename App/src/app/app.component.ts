d8708f873334811ced4ccf8e7f6b2f62ac0e20a9
unknown <ispg2022100651@ispgaya.pt>
2024-06-12 18:53:55 +0100

    #001

--- App/src/app/app.component.ts
+++ App/src/app/app.component.ts
@@ -27,6 +27,7 @@ export class AppComponent
   run_pace : TriathlonPace[] = [new TriathlonPace(0)];
 
   total_time : TriathlonTime = new TriathlonTime(0,0,0);
+  
   goal_result : TriathlonGoal = new TriathlonGoal(
     new TriathlonDistance(0,0,0), 
     {swim: new TriathlonTime(0,0,0), run: new TriathlonTime(0,0,0), bike: new TriathlonTime(0,0,0)}, 
@@ -315,7 +316,7 @@ export class AppComponent
     this.update_run_pace();
     this.update_swim_pace();
   }
- 
+
   // Pace input changed
   update_bike_time() {
     const pace_seconds = this.bike_pace[0]['speed'];
